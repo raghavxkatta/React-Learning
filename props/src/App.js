2113303d3bdf571a661216e2ed814a1e2ac94580
Raghav Katta <Raghavkatta2005@gmail.com>
2024-10-01 09:58:19 +0530

    Good {timeOfDay}

---
+++ props/src/App.js
@@ -0,0 +1,25 @@
+
+import './App.css';
+
+function App() {
+  const date=new Date()
+  const hours= date.getHours()
+  let timeOfDay
+
+  if (hours<12){
+    timeOfDay="morning"
+  }
+  else if (hours>=12&& hours<17){
+timeOfDay="afternoon"
+  }
+  else{
+    timeOfDay="night"
+  }
+  return (
+    <div className="App">
+      <h1>Good {timeOfDay} </h1>
+    </div>
+  );
+}
+
+export default App;
