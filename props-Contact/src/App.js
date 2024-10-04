0191f920f74195790c91d008841024db7061719d
Raghav Katta <Raghavkatta2005@gmail.com>
2024-10-04 12:59:21 +0530

    props-hardcoded data

--- props-Contact/src/App.js
+++ props-Contact/src/App.js
@@ -4,10 +4,27 @@ import Contact from "./components/Contact"
 function App() {
   return (
     <div className="Contacts">
-<Contact />    
-<Contact />    
-<Contact />    
-<Contact />      
+<Contact
+img="./images/mr-whiskerson.png"
+name="Mr. Whiskerson"
+phone="(212) 555-1234" 
+mail="mr.whiskaz@catnap.meow"/> 
+   
+<Contact
+ img="./images/fluffykins.png"
+ name="Fluffykins" 
+ phone="(212) 555-2345"
+ mail="fluff@me.com"  />  
+<Contact 
+img="./images/felix.png"
+name="Felix"
+phone="(212) 555-4567"
+mail="thecat@hotmail.com"/>    
+<Contact 
+img="./images/pumpkin.png"
+name="Pumpkin"
+phone="(0800) CAT KING"
+mail="pumpkin@scrimba.com"/>      
 </div>
   );
   
--- props-Contact/src/components/Contact.js
+++ props-Contact/src/components/Contact.js
@@ -1,19 +1,23 @@
-import React from "react"
-import Whisker from "../assets/mr-whiskerson.png"
-import PhoneIcon from "../assets/phone-icon.png"
-import MailIcon from "../assets/mail-icon.png"
-export default function Contact(){
-    return(
-    <div className="contact-card">
-    <img src={Whisker} alt=""/>
-    <h3>Mr. Whiskerson</h3>
-    <div className="info-group">
-        <img src={PhoneIcon} alt="" />
-        <p>(212) 555-1234</p>
-    </div>
-    <div className="info-group">
-        <img src={MailIcon} alt="" />
-        <p>mr.whiskaz@catnap.meow</p>
-    </div>
-    </div>
-)}
+import React from "react";
+import Whisker from "../assets/mr-whiskerson.png";
+import PhoneIcon from "../assets/phone-icon.png";
+import MailIcon from "../assets/mail-icon.png";
+/* AS i SAVE THIS IT WILL LOG IN ALL THE DETAILS ON THE CONSOLE 4 TIMES WITH THE DETAILS OF ALL THE OTHER ITEMS TOO 
+THIS IS BECAUSE WE HAVE 4 CONTACT INSTANCES, Every time react rendered one of the contacts,it ran the console log line inside of the code of that component */
+export default function Contact(props) {
+    console.log(props)
+    return (
+        <div className="contact-card">
+            <img src={Whisker} alt="" />
+            <h3>Mr. Whiskerson</h3>
+            <div className="info-group">
+                <img src={PhoneIcon} alt="" />
+                <p>(212) 555-1234</p>
+            </div>
+            <div className="info-group">
+                <img src={MailIcon} alt="" />
+                <p>mr.whiskaz@catnap.meow</p>
+            </div>
+        </div>
+    );
+}
