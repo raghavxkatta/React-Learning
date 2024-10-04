f08a94a88530b69787f4f87065f75dac2ad6ee8d
Raghav Katta <Raghavkatta2005@gmail.com>
2024-10-04 16:59:17 +0530

    destructuring props

--- props-Contact/src/components/Contact.js
+++ props-Contact/src/components/Contact.js
@@ -4,19 +4,18 @@ import PhoneIcon from "../assets/phone-icon.png";
 import MailIcon from "../assets/mail-icon.png";
 /* AS i SAVE THIS IT WILL LOG IN ALL THE DETAILS ON THE CONSOLE 4 TIMES WITH THE DETAILS OF ALL THE OTHER ITEMS TOO 
 THIS IS BECAUSE WE HAVE 4 CONTACT INSTANCES, Every time react rendered one of the contacts,it ran the console log line inside of the code of that component */
-export default function Contact(props) {
-    console.log(props)
-    return (
+export default function Contact({img,name,phone,mail}) {
+        return (
         <div className="contact-card">
-            <img src={props.img} alt="" />
-            <h3>{props.name}</h3>
+            <img src={img} alt="" />
+            <h3>{name}</h3>
             <div className="info-group">
                 <img src={PhoneIcon} alt="" />
-                <p>{props.phone}</p>
+                <p>{phone}</p>
             </div>
             <div className="info-group">
                 <img src={MailIcon} alt="" />
-                <p>{props.mail}</p>
+                <p>{mail}</p>
             </div>
         </div>
     );
