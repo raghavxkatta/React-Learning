b737ac6acbb22bf4e4e0b0701ecd98f7eee04dea
Raghav Katta <Raghavkatta2005@gmail.com>
2024-10-05 07:45:49 +0530

    props-practive-2

--- jokes/src/components/Jokes.js
+++ jokes/src/components/Jokes.js
@@ -4,7 +4,8 @@ export default function Joke(props){
     console.log(props)
     return(
         <div>
-            {/* this says of props.setup is a truthy value then render the thing on the right side of my and operator, If it's a falsie value it won't render anything */}
+            {/* This basically means when prop-setup does not existit has a display of none and vice versa */}
+                <h3 style={{display:props.Setup ? "block":"none"}}>Setup: {props.Setup}</h3>
         {props.Setup&&<h3>Setup:{props.Setup}</h3>}
         <p>{props.Punchline}</p>
         </div>
