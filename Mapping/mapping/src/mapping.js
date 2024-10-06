6252114a9a471e91569a9e775be68a830806d546
Raghav Katta <Raghavkatta2005@gmail.com>
2024-10-06 13:42:12 +0530

    mapping-challenge-2

--- Mapping/mapping/src/mapping.js
+++ Mapping/mapping/src/mapping.js
@@ -1,4 +1,21 @@
+/*
+Challenge 1:
+Given an array of numbers, return an array of each number, squared
+*/
 const nums=[1,2,3,4,5]
-nums.map(function square(num){
+const squared=nums.map(function square(num){
 return num*num /* whatever I return from this function will get placed at the exact same index as the original item with whatever kind of modification I make to it */
 })
+console.log(squared)
+
+/*
+Challenge 2:
+Given an array of strings, return an array where 
+the first letter of each string is capitalized
+*/
+const names=["alice","bob","charlie","danielle"]
+const capitalized=names.map(name=>{
+    return name[0].toUpperCase() + name.slice(1)/* since we did not provide a second parameter therefore it would go on from the second letter to the very end */
+
+})
+console.log(capitalized)
