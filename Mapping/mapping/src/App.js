4b14cd6e4307ba22b6d3bf49913c3026f3881a45
Raghav Katta <Raghavkatta2005@gmail.com>
2024-10-06 13:55:23 +0530

    react renders arrays

--- Mapping/mapping/src/App.js
+++ Mapping/mapping/src/App.js
@@ -1,9 +1,18 @@
 import './App.css';
 
 function App() {
+  const colors = [
+    <h3>Red</h3>, 
+    <h3>Orange</h3>, 
+    <h3>Yellow</h3>,
+    <h3>Green</h3>,
+    <h3>Blue</h3>,
+    <h3>Indigo</h3>,
+    <h3>Violet</h3>
+]
   return (
     <div className="App">
-
+   {colors}
     </div>
   );
 }
