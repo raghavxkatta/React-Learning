f70ea77e40e95f78bb7ff65966ad597bba99fe8d
Raghav Katta <Raghavkatta2005@gmail.com>
2024-10-06 13:15:34 +0530

    array.map-challenge-1

---
+++ Mapping/mapping/src/mapping.js
@@ -0,0 +1,4 @@
+const nums=[1,2,3,4,5]
+nums.map(function square(num){
+return num*num /* whatever I return from this function will get placed at the exact same index as the original item with whatever kind of modification I make to it */
+})
