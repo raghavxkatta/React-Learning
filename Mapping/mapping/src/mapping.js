880eeaee0ac42fe8e0b6614c4103c237a2fbf265
Raghav Katta <Raghavkatta2005@gmail.com>
2024-10-06 13:42:55 +0530

    mapping-challenge-3

--- Mapping/mapping/src/mapping.js
+++ Mapping/mapping/src/mapping.js
@@ -19,3 +19,18 @@ const capitalized=names.map(name=>{
 
 })
 console.log(capitalized)
+
+/*
+Challenge 3:
+Given an array of strings, return an array of strings that wraps each
+of the original strings in an HTML-like <p></p> tag.
+
+E.g. given: ["Bulbasaur", "Charmander", "Squirtle"]
+return: ["<p>Bulbasaur</p>", "<p>Charmander</p>", "<p>Squirtle</p>"]
+*/
+
+const pokemon = ["Bulbasaur", "Charmander", "Squirtle"]
+const htmlTags= pokemon.map(mons=>{
+    return `<p>${mons}</p>`
+})
+console.log(htmlTags)
