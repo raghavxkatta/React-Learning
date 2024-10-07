0eb8ce56e1de5c2614ade6f5460ee295d0debf36
Raghav Katta <Raghavkatta2005@gmail.com>
2024-10-07 10:52:28 +0530

    jokes using mapping

--- jokes/src/App.js
+++ jokes/src/App.js
@@ -1,29 +1,15 @@
-import './App.css';
-import Jokes from "./components/Jokes"
-function App() {
-  return (
-    <div className="Jokes">
-      <Jokes Setup="got my daughter a fridge for her birthday."
-        Punchline="I can't wait to see her face light up when she opens it."
-      />
-      <Jokes
-        Setup="How did the hacker escape the police?"
-        Punchline="He just ransomware!"
-      />
-      <Jokes
-        Setup="Why don't pirates travel on mountain roads?"
-        Punchline="Scurvy."
-      />
-      <Jokes
-        Setup=" Why do bees stay in the hive in the winter?"
-        Punchline="Swarm."
-      />
-      <Jokes
-        Setup=" What's the best thing about Switzerland?"
-        Punchline="I don't know, but the flag is a big plus"
-      />
+  import './App.css';
+  import Jokes from "./components/Jokes"
+  import jokesData from './jokesData';  
+  function App() {
+    const jokeElements= jokesData.map(joke=>{
+  return <Jokes setup={joke.setup} punchline={joke.punchline} />
+    })  
+  return(
+    <div>
+      {jokeElements}
     </div>
-  );
-}
+  )
+  }
 
-export default App;
+  export default App;
--- jokes/src/components/Jokes.js
+++ jokes/src/components/Jokes.js
@@ -7,6 +7,7 @@ export default function Joke(props){
             {/* This basically means when prop-setup does not existit has a display of none and vice versa */}
                 <h3 style={{display:props.Setup ? "block":"none"}}>Setup: {props.Setup}</h3>
         <p>{props.Punchline}</p>
+        <hr/>
         </div>
     )
 }
