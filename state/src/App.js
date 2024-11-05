f7f914e91e9a3ccfc6790c4da6007a56d41e8c98
Raghav Katta <Raghavkatta2005@gmail.com>
2024-11-06 04:46:17 +0530

    added mouseHover

--- state/src/App.js
+++ state/src/App.js
@@ -1,10 +1,11 @@
 import logo from './logo.svg';
 import './App.css';
+import HandleClick from '../src/components/HandleClick'
 
 function App() {
   return (
     <div className="App">
-     
+     <HandleClick/>
     </div>
   );
 }
--- state/src/components/HandleClick.js
+++ state/src/components/HandleClick.js
@@ -3,10 +3,18 @@ export default function Handle(){
     function handleClick(){
         console.log("I was clicked")
     }
+
+    function mouseHover(){
+        console.log("MouseOver")
+    }
 return(
     <div className="container">
-<img src="https://picsum.photos/640/360" alt="random"/>
-<button onClick={handleClick}>Click Me</button>{/* we will not enter the function() but rather as a value function without brackets because it then runs the function immediately as soon as the code reaches the particular line of code */}
+<img 
+src="https://picsum.photos/640/360" 
+onMouseOver={mouseHover}
+alt="random" 
+/>
+<button onMouseEnter={handleClick}>Click Me</button>{/* we will not enter the function() but rather as a value function without brackets because it then runs the function immediately as soon as the code reaches the particular line of code */}
     </div>
 )
 }
