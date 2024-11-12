c6bdace51e5e8de5bc24a690a2366ead90598f57
Raghav Katta <Raghavkatta2005@gmail.com>
2024-11-12 11:53:07 +0530

    intro to useState

---
+++ state/src/components/First.js
@@ -0,0 +1,14 @@
+import React from "react";
+export default function App(){
+    const result = React.useState("Yes")/* this makes an array whose first value is "Yes" */
+    console.log(result)
+    return (
+        <div className="state">
+            <h1 className="state--title">Is state important to know?</h1>
+            <div className="state--value">
+                <h1>{result[0]}</h1>
+            </div>
+        </div>
+    )
+}
+
