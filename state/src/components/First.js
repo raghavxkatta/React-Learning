3ea55d6f028f5169a23ab401553548f33f58059e
Raghav Katta <Raghavkatta2005@gmail.com>
2024-11-12 11:58:18 +0530

    useState Array Destructuring

--- state/src/components/First.js
+++ state/src/components/First.js
@@ -1,12 +1,12 @@
 import React from "react";
 export default function App(){
-    const result = React.useState("Yes")/* this makes an array whose first value is "Yes" */
-    console.log(result)
+    const {isImportant,func} = React.useState("Yes")/* this makes an array whose first value is "Yes", the array has two things one the state variable */
+    console.log(isImportant)
     return (
         <div className="state">
             <h1 className="state--title">Is state important to know?</h1>
             <div className="state--value">
-                <h1>{result[0]}</h1>
+                <h1>{isImportant}</h1>
             </div>
         </div>
     )
