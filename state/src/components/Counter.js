ff3f1680381e2833b489b1718b42fffc878b76a8
Raghav Katta <Raghavkatta2005@gmail.com>
2024-11-13 23:56:39 +0530

    counter-2

--- state/src/components/Counter.js
+++ state/src/components/Counter.js
@@ -1,21 +1,21 @@
 import React from "react";
 export default function Counter() {
-    const [count,setCount]=React.useState(0)
+    const [count, setCount] = React.useState(0)
     /**
      *  if you ever need the old value of state
      * to help you determine the new value of state,
      * you should pass a callback function to your
      * state setter function instead of using
      * state directly. This callback function will
-     * receive the old value of state as its parameter,
+     * receive the old value of state as its parameter , 
      * which you can then use to determine your new
      * value of state.
      */
-    function add(){
-        setCount(prevCount=>prevCount+1)
+    function add() {
+        setCount(prevCount => prevCount + 1)
     }
-    function subtract(){
-       setCount(prevCount=>prevCount-1)
+    function subtract() {
+        setCount(prevCount => prevCount - 1)
     }
     return (
         <div className="Counter">
